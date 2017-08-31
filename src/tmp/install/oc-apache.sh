0141a1fadc43853da5e00f3dc5765f7b6173debf
Joern Ott <joern.ott@ott-consult.de>
2017-08-31 16:59:38 +0200

    Install mod_ssl as well

--- src/tmp/install/oc-apache.sh
+++ src/tmp/install/oc-apache.sh
@@ -6,5 +6,5 @@ curl -sSo /tmp/install/functions.sh https://raw.githubusercontent.com/joernott/d
 source /tmp/install/functions.sh
 
 add_repos IUS EPEL
-install_software httpd24u-${APACHE_VERSION}
+install_software httpd24u-${APACHE_VERSION} httpd24u-mod_ssl-${APACHE_VERSION}
 cleanup
